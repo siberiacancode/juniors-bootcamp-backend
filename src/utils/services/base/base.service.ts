9c5e03273dd4ee59e3528edcbc63b90e7783f862
debabin <dmitriybabinup@gmail.com>
2026-04-03 13:39:53 +0700

    fix page for otps

--- src/utils/services/base/base.service.ts
+++ src/utils/services/base/base.service.ts
@@ -5,17 +5,27 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class BaseService<Doc extends Document> {
-  constructor(private readonly model: Model<Doc>) {}
+  create!: Model<Doc>['create'];
+  insertMany!: Model<Doc>['insertMany'];
+  find!: Model<Doc>['find'];
+  findById!: Model<Doc>['findById'];
+  findOne!: Model<Doc>['findOne'];
+  updateOne!: Model<Doc>['updateOne'];
+  updateMany!: Model<Doc>['updateMany'];
+  findOneAndUpdate!: Model<Doc>['findOneAndUpdate'];
+  delete!: Model<Doc>['deleteOne'];
 
-  create = this.model.create;
-  insertMany = this.model.insertMany;
-  find = this.model.find;
-  findById = this.model.findById;
-  findOne = this.model.findOne;
-  updateOne = this.model.updateOne;
-  updateMany = this.model.updateMany;
-  findOneAndUpdate = this.model.findOneAndUpdate;
-  delete = this.model.deleteOne;
+  constructor(private readonly model: Model<Doc>) {
+    this.create = this.model.create.bind(this.model);
+    this.insertMany = this.model.insertMany.bind(this.model);
+    this.find = this.model.find.bind(this.model);
+    this.findById = this.model.findById.bind(this.model);
+    this.findOne = this.model.findOne.bind(this.model);
+    this.updateOne = this.model.updateOne.bind(this.model);
+    this.updateMany = this.model.updateMany.bind(this.model);
+    this.findOneAndUpdate = this.model.findOneAndUpdate.bind(this.model);
+    this.delete = this.model.deleteOne.bind(this.model);
+  }
 
   async findAndDelete(filter: QueryFilter<Doc>) {
     const documents = await this.model.find(filter);
@@ -23,7 +33,7 @@ export class BaseService<Doc extends Document> {
     return { ...deletedInfo, documents };
   }
 
-  total(...params: Parameters<typeof this.model.countDocuments>) {
+  total(...params: Parameters<Model<Doc>['countDocuments']>) {
     return this.model.countDocuments(...params);
   }
 }
