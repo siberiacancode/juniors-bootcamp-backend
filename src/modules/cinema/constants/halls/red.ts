f80c49ab4ae540a325a1a8ecf735e425edcd0319
nizhdanov <zhdanov.delovayakolbasa@gmail.com>
2026-06-02 18:50:12 +0500

    rename econom

--- src/modules/cinema/constants/halls/red.ts
+++ src/modules/cinema/constants/halls/red.ts
@@ -4,49 +4,49 @@ export const red = [
   [
     { price: 0, type: FilmHallCellType.BLOCKED },
     { price: 0, type: FilmHallCellType.BLOCKED },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
     { price: 0, type: FilmHallCellType.BLOCKED },
     { price: 0, type: FilmHallCellType.BLOCKED }
   ],
   [
     { price: 0, type: FilmHallCellType.BLOCKED },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
     { price: 0, type: FilmHallCellType.BLOCKED }
   ],
   [
-    { price: 100, type: FilmHallCellType.ECONOMY },
+    { price: 100, type: FilmHallCellType.ECONOM },
     { price: 200, type: FilmHallCellType.COMFORT },
     { price: 200, type: FilmHallCellType.COMFORT },
     { price: 200, type: FilmHallCellType.COMFORT },
     { price: 200, type: FilmHallCellType.COMFORT },
-    { price: 100, type: FilmHallCellType.ECONOMY }
+    { price: 100, type: FilmHallCellType.ECONOM }
   ],
   [
-    { price: 100, type: FilmHallCellType.ECONOMY },
+    { price: 100, type: FilmHallCellType.ECONOM },
     { price: 200, type: FilmHallCellType.COMFORT },
     { price: 200, type: FilmHallCellType.COMFORT },
     { price: 200, type: FilmHallCellType.COMFORT },
     { price: 200, type: FilmHallCellType.COMFORT },
-    { price: 100, type: FilmHallCellType.ECONOMY }
+    { price: 100, type: FilmHallCellType.ECONOM }
   ],
   [
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY }
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM }
   ],
   [
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY },
-    { price: 100, type: FilmHallCellType.ECONOMY }
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM },
+    { price: 100, type: FilmHallCellType.ECONOM }
   ]
 ];
